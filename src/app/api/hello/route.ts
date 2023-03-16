4dfea502dea6a13738b52bbac3a369d81d1575e5
Ivan D <bad_vano@list.ru>
2023-03-16 06:50:11 +0300

    basic linting and prettier configuration

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, res: Response): Promise<NextResponse> {
+  return NextResponse.json({ name: 'John' });
 }
