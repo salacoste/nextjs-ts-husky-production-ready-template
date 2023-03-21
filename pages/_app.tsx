dc15c2d429646b61bda991e4d8057ef8ea4e2933
Ivan D <bad_vano@list.ru>
2023-03-21 15:36:14 +0300

    apolloClient with queryHook are done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from '@/lib/apollo/apollo';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <>
+        <Component {...pageProps} />;
+      </>
+    </ApolloProvider>
+  );
 }
--- pages/tailwindCssPage.tsx
+++ pages/tailwindCssPage.tsx
@@ -1,11 +1,35 @@
 import styles from '@/styles/TailwindPage.module.css';
 
+import { gql, useQuery } from '@apollo/client';
+import { getAllTools } from '@/graphql/queries';
+import { Tool } from '@/lib/dump-data/types';
+
 export default function TailwindPage(props: {}) {
-  return (
-    <>
-      <div className={`${styles.h1} p-5`}>
-        <h1 className="text-3xl text-bold text-center">Hello World</h1>
+  const { loading, error, data } = useQuery(getAllTools);
+  console.log(data, error, loading);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error :(
+        {error.message}
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="container">
+      <h1 className="text-3xl text-center my-4">Tools</h1>
+      <ul className="flex-1">
+        {data?.getTools?.map((tool: Tool) => (
+          <li key={tool.id} className="w-full mb-3 p-2">
+            {tool.name} : {tool.description}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
